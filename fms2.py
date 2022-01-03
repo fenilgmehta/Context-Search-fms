8de0e161e2c18c7303a319105ca502bd6b55a547
Fenil Mehta <fenilgmehta@gmail.com>
2022-01-03 07:49:44 +0530

    ADD `fms2.py` to Refactor `fms.py` code, Add new features, Improve performance

---
+++ fms2.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+#!/usr/bin/python3
+
+# Copyright (C) 2021-2022 Fenil Mehta <fenilgmehta@gmail.com>
+# All Rights Reserved.
+
+def my_main():
+    pass
+
+
+if __name__ == '__main__':
+    my_main()
